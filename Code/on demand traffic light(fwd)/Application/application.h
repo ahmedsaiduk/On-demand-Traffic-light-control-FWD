6511d61798208ca02e71bccd78b193debd671936
Ahmed <ahmedsaiduk@gmail.com>
2023-02-25 01:26:49 +0200

    remove mcal from Application layer

--- Code/on demand traffic light(fwd)/Application/application.h
+++ Code/on demand traffic light(fwd)/Application/application.h
@@ -14,8 +14,6 @@
 #define F_CPU 1000000
 #include "../ECUAL/LED/led.h"
 #include "../ECUAL/BTN/btn.h"
-#include "../MCAL/Interrupt/interrupt.h"
-#include "../MCAL/Timer/timer.h"
 
 void app_init();
 void app_start();
